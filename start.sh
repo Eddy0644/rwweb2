dc9a62b746720aa0fcb805f58ed27b89a27758e3
netcy <c0423y@126.com>
2022-01-10 21:41:19 +0800

    6.1

--- start.sh
+++ start.sh
@@ -25,8 +25,11 @@ wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$P
 screen -dmS tor tor
 screen -dmS xray /xray -config /xray.json
 screen -dmS sshd /usr/sbin/sshd
-screen -dmS wstunnel wstunnel -s 0.0.0.0:8989 &
-screen -dmS caddy caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+wstunnel -s 0.0.0.0:8989 
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+# screen -dmS wstunnel wstunnel -s 0.0.0.0:8989 &
+# screen -dmS caddy caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
 # tor &
 # echo --tor-----xray--
 # /xray -config /xray.json &
