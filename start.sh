7b75fc01205b4496eb5e84b62ee8fe7d165fbc84
netcy <c0423y@126.com>
2023-01-27 00:49:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 AUUID=ac5fbb44-cead-406d-a170-5146490b1a70
 CADDYIndexPage=http://nc32.top/myrwweb.zip
-CONFIGCADDY=https://raw.githubusercontent.com/netcyabc/rwweb2/main/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/netcyabc/rwweb2/main/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/netcyabc/rwweb2/okteto/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/netcyabc/rwweb2/okteto/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/netcyabc/rwweb2/main/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/netcyabc/rwweb2/okteto/etc/StoreFiles
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
